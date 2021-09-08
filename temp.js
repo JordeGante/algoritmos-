6919468d677e48434c2bc66adc5d1890cd949c7f
Jorman <jorman_1321124121@uptecamac.com>
2021-09-08 18:46:49 -0500

    que onda

---
+++ temp.js
@@ -0,0 +1,16 @@
+/*var numero1=10; 
+var numero2=20;
+var texto="hola";
+if(numero1<numero2){
+    console.log(numero1 +"es menor" + numero2);
+
+}
+*/
+var persona1="juan";
+var edad=18;
+
+if(edad>=18){
+    console.log("la persona "+ persona1+" es mayor de edad");
+}else{
+    console.log("la persona"+persona1+" es menor de edad");
+}
