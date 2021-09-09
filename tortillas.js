3b4060a72cb7165c19d4bdefd2c75b2e85c17da2
Jorman <jorman_1321124121@uptecamac.com>
2021-09-08 19:04:47 -0500

    lasssss tortillas

---
+++ tortillas.js
@@ -0,0 +1,12 @@
+var tortillas=18;
+var dinero=20;
+
+console.log("paso 1:salir de casa");
+console.log("paso 2:dirigirse a la tortilleria");
+console.log("paso 3:comprar tortillas");
+if(tortillas>=dinero){
+    console.log("llevar las tortillas a mi casa ");
+}
+else{
+    console.log("me voy a casa sin tortillas");
+}
